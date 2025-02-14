00de56da960c515c39f41e6762abe548c6d4d672
EduardoDev <138069501+edusabi@users.noreply.github.com>
2025-02-13 23:43:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -194,40 +194,6 @@ console.log(valor)
 }
 */
 
-//////////////////////////////////////////////////função while e do while
-//while
-/*
-function random(min, max) {
-    const r = Math.random() * (max - min) + min; 
-    return Math.floor(r);
-  }
-  
-  
-  let rand = random(100, 0);
-  
-  while (rand !== 10) {
-    rand = random(100, 0);
-    console.log(rand);
-  }
-
-//Exercicio 1
-
-*/
-
-///////////////////////////////////////////////////////do while
-
-/*
-let result = ''
-let i = 0;
-
-do {
-  i = i + 1;
-  result = result + ' ' + i;
-} while (i < 5);
-
-console.log(result);
-*/
-
 //Exercicio 2
 
 /*const ePaisagem = (larg,altr) => larg > altr
