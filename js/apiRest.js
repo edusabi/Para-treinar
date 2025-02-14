de74b4dc592008e1104588a1178128304322ba62
EduardoDev <138069501+edusabi@users.noreply.github.com>
2025-02-13 23:28:58 -0300

    Create apiRest.js

---
+++ js/apiRest.js
@@ -0,0 +1,18 @@
+//////////////USANDO FETCH API (GET)
+/*
+async function carregaPagina(el){
+
+  try{
+    const href = el.getAttribute('href')
+    const response = await fetch(href)
+
+    if(response.status !== 200) throw new Error('ERROR 404!')
+
+    const html = await response.text()
+    carregaResultado(html)
+  }catch(e){
+    console.log(e)
+  }
+
+}
+*/
